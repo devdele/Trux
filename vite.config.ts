94216dc6b8bac7158b34070ea3fe98700ee8d0d0
devdele <ayodeletoluwani71@gmail.com>
2025-05-13 17:08:30 +0100

    latest chnage

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/CryptoLandingPage",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
+})
